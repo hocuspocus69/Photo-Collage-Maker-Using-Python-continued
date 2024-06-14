49cf13cdf30ea5956bd9a9411dd2d1cb61a297d3
hocuspocus69 <hocuspocus69@hotmail.com>
2024-06-14 14:29:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ st.markdown(
              # background-image: url("https://4kwallpapers.com/images/wallpapers/embarrassment-3840x2160-16536.jpeg");
              # background-attachment: fixed;
              # background-size: cover
-             :black-background
-             :white
+             background: #242424 !important
          }}
          </style>
          """,
